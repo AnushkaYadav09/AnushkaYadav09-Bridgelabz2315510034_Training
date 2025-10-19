da98ba3599736844b8ef610e2e9b8e246b06be21
AnushkaYadav09 <yadavanushka759@gmail.com>
2025-10-19 22:42:53 +0530

    Delete java_samples directory

--- java_samples/Program5/MainDemo5.java
+++
@@ -1,7 +0,0 @@
-public class MainDemo5 {
-    public static void main(String[] args) {
-        Student s1 = new Student("Alice", 101, "A");
-        s1.displayDetails();
-        Student.displayTotalStudents();
-    }
-}
--- java_samples/Program7/MainDemo7.java
+++
@@ -1,7 +0,0 @@
-public class MainDemo7 {
-    public static void main(String[] args) {
-        Patient p1 = new Patient("John", 45, "Flu", 1);
-        p1.displayDetails();
-        System.out.println("Total Patients: " + Patient.getTotalPatients());
-    }
-}
